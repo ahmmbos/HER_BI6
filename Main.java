641ce812de9196db3966cb7bcea3228d76db2a21
Amber Bos <ahmm.bos@student.han.nl>
2023-03-10 11:48:00 +0100

    changed head documentation for better readability

--- Main.java
+++ Main.java
@@ -1,15 +1,13 @@
-
 /**
  * <h1>Herkansing Bio-informatica, blok 6</h1>
- * Application that reads (.gbff)-files from Genbank and lets the user search
+ * <p>Application that reads (.gbff)-files from Genbank and lets the user search
  * through authors and/or publications. Authors and publications are connected:
- * the application lets the user "surf" through the authors and publications.
- *
- * <p>Study-project that does the following:
- * 1. Read in the files (.gbff)
- * 2. Make it possible to search for an author in the list
- * 3. Make it possible to go to publication via author (and vice versa)
- * 4. Make it possible to select an item and write all associated data from it, to a file</p>
+ * the application lets the user "surf" through the authors and publications.</p>
+ * The application in a nutshell:<br>
+ * 1. Read in the files (.gbff)<br>
+ * 2. Make it possible to search for an author in the list<br>
+ * 3. Make it possible to go to publication via author (and vice versa)<br>
+ * 4. Make it possible to select an item and write all associated data from it, to a file<br>
  *
  * @author Amber Bos
  * @since 10-03-2023
