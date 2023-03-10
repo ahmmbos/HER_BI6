b72908caa46141a78d2dfe3f10affd201f979a58
Amber Bos <ahmm.bos@student.han.nl>
2023-03-10 11:43:37 +0100

    main added + head documentation

---
+++ Main.java
@@ -0,0 +1,19 @@
+
+/**
+ * <h1>Herkansing Bio-informatica, blok 6</h1>
+ * Application that reads (.gbff)-files from Genbank and lets the user search
+ * through authors and/or publications. Authors and publications are connected:
+ * the application lets the user "surf" through the authors and publications.
+ *
+ * <p>Study-project that does the following:
+ * 1. Read in the files (.gbff)
+ * 2. Make it possible to search for an author in the list
+ * 3. Make it possible to go to publication via author (and vice versa)
+ * 4. Make it possible to select an item and write all associated data from it, to a file</p>
+ *
+ * @author Amber Bos
+ * @since 10-03-2023
+ */
+public class Main {
+    // do something
+}
